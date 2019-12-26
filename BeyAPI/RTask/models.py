716237c6e1cd2f5f9dcc5e77fb8a33d629bac5a1
nikomar <markadeev@ukr.net>
2019-12-26 23:24:24 +0200

    commit

--- BeyAPI/RTask/models.py
+++ BeyAPI/RTask/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class rtask(models.Model):
+    ua = models.CharField(max_length=200)
+    ru = models.CharField(max_length=200)
+    en = models.CharField(max_length=200)
+    nl = models.CharField(max_length=200)
+    de = models.CharField(max_length=200)
+
+
+
